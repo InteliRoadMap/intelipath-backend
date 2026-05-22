dedcd7ffd839c3d001da202d42e0a5acade343ba
Dang Phuoc Vinh <dpvinh30092005@gmail.com>
2026-05-22 15:39:17 +0700

    chore: setup backend project

--- src/main/java/com/inteliroadmap/backend/domain/entity/User.java
+++ src/main/java/com/inteliroadmap/backend/domain/entity/User.java
@@ -1,4 +1,13 @@
 package com.inteliroadmap.backend.domain.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private int id;
+    private String name;
+    private String password;
+    private String email;
 }
