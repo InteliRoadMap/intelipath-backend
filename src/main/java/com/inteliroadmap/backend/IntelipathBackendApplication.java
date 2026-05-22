a2e4311c3987d262989f06608853be61c486ea06
Dang Phuoc Vinh <dpvinh30092005@gmail.com>
2026-05-22 13:44:59 +0700

    chore: init Spring Boot project

---
+++ src/main/java/com/inteliroadmap/backend/IntelipathBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inteliroadmap.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelipathBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelipathBackendApplication.class, args);
+    }
+
+}
