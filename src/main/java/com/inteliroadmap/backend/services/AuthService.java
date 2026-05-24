4b9019682da9f743c5d8a60d9ea8dff19756cd11
Dang Phuoc Vinh <dpvinh30092005@gmail.com>
2026-05-24 20:04:40 +0700

    chore: adjust file

--- src/main/java/com/inteliroadmap/backend/services/AuthService.java
+++ src/main/java/com/inteliroadmap/backend/services/AuthService.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
