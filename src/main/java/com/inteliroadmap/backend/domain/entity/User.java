fd5f043f978491dbfc5bbdbb3a535cfe60c19a04
Hau <thanhhau2110@gmail.com>
2026-05-29 15:45:56 +0700

    pull request from main

--- src/main/java/com/inteliroadmap/backend/domain/entity/User.java
+++ src/main/java/com/inteliroadmap/backend/domain/entity/User.java
@@ -32,9 +32,6 @@ public class User {
     @Column(name = "user_id")
     private UUID userId;
 
-    @Column(nullable = false)
-    private String password;
-
     @Column(name = "full_name")
     private String fullName;
 
@@ -69,13 +66,6 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<OauthAccount> oauthAccounts;
-
-
-    @Column(name = "otp")
-    private String otp;
-
-    @Column(name = "otp_expiry")
-    private LocalDateTime otpExpiry;
 
 //    // 1 User có nhiều records ở bảng con
 //    // mappedBy = tên field trong class con trỏ ngược lại User
