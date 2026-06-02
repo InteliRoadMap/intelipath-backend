e3da0753fb5aa2a868abf770e95a32894e06a6e6
Hau <thanhhau2110@gmail.com>
2026-06-02 14:05:53 +0700

    feat: add career services

---
+++ src/main/java/com/inteliroadmap/backend/repositories/CareerRoleRepository.java
@@ -0,0 +1,13 @@
+package com.inteliroadmap.backend.repositories;
+
+import com.inteliroadmap.backend.domain.entity.CareerRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CareerRoleRepository extends JpaRepository<CareerRole, UUID> {
+    Optional<CareerRole> findByCareerName(String careerName);
+}
