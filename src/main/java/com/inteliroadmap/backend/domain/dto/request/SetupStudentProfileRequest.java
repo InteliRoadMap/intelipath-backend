239908a2a37844070cf8c7b9298c6b2db0fd72bb
Hau <thanhhau2110@gmail.com>
2026-06-02 14:07:07 +0700

    fix: divide setup func

---
+++ src/main/java/com/inteliroadmap/backend/domain/dto/request/SetupStudentProfileRequest.java
@@ -0,0 +1,26 @@
+package com.inteliroadmap.backend.domain.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SetupStudentProfileRequest {
+
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    private String university;
+
+    private String year_of_admission;
+
+    @NotBlank(message = "Major is required")
+    private String major;
+}
---
+++ src/main/java/com/inteliroadmap/backend/domain/dto/request/SetupUserProfileRequest.java
@@ -0,0 +1,24 @@
+package com.inteliroadmap.backend.domain.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SetupUserProfileRequest {
+
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    private String yob;
+
+    private String bio;
+}
