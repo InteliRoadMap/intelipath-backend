fb0085e90dc7cfefb14c9314c2a1df4a4d288a67
Hau <thanhhau2110@gmail.com>
2026-06-17 15:24:30 +0700

    fix: minor code fix

--- src/main/java/com/inteliroadmap/backend/repositories/StudentSkillRepository.java
+++ src/main/java/com/inteliroadmap/backend/repositories/StudentSkillRepository.java
@@ -6,10 +6,12 @@ import com.inteliroadmap.backend.mappers.DatasetMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface StudentSkillRepository extends JpaRepository<StudentSkill, UUID> {
     List<StudentSkill> findByStudent(Student student);
 
